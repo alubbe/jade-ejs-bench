9b2cf414a3c04894f8a1ae1f658208f609a34fed
Andreas Lubbe <git@lubbe.org>
2015-05-28 18:20:16 +0200

    added a lot more benchmarks

--- ejs.js
+++ ejs.js
@@ -2,31 +2,27 @@ var ejs   = require('ejs')
 var fs    = require('fs')
 
 var ejsCompiled   = []
-var locals        = [
-  {
-    things: [
-      { a: 'blah', b: 'halb' }
-    , { a: 'clah', b: 'halc' }
-    , { a: 'clah', b: 'halc' }
-    , { a: 'blah', b: 'halb' }
-    , { a: 'clah', b: 'halc' }
-    , { a: 'clah', b: 'halc' }
-    , { a: 'clah', b: 'halc' }
-    , { a: 'blah', b: 'halb' }
-    , { a: 'blah', b: 'halb' }
-    , { a: 'clah', b: 'halc' }
-    , { a: 'blah', b: 'halb' }
-    ]
-  }
-, {}
+var things = [
+    { a: 'blah', b: 'halb' }
+  , { a: 'clah', b: 'halc' }
+  , { a: 'clah', b: 'halc' }
+  , { a: 'blah', b: 'halb' }
+  , { a: 'clah', b: 'halc' }
+  , { a: 'clah', b: 'halc' }
+  , { a: 'clah', b: 'halc' }
+  , { a: 'blah', b: 'halb' }
+  , { a: 'blah', b: 'halb' }
+  , { a: 'clah', b: 'halc' }
+  , { a: 'blah', b: 'halb' }
 ]
+var locals = [{things:things}, {things:things}, {things:things}, {things:things}, {}]
 
 // Jade doesn't use with () {} so make it fair
 // Jade also doesn't support dynamic inclusion
 var ejsOpts  = { _with: false }
 var ejsOpts  = { compileDebug: false, _with: false }
 
-var files = fs.readdirSync('jade').map(function (f) {
+var files = fs.readdirSync('ejs').map(function (f) {
   return f.split('.')[0]
 })
 
@@ -41,8 +37,8 @@ for (var i = 0; i < files.length; i ++) {
   var local = locals[i]
 
   var fn = ejsCompiled[i]
-  for (var j = 0; j < 1000000; j ++) fn(local)
+  for (var j = 0; j < 200000; j ++) fn(local)
   console.time(f)
-  for (var j = 0; j < 10000000; j ++) fn(local)
+  for (var j = 0; j < 2000000; j ++) fn(local)
   console.timeEnd(f)
 }
--- jade.js
+++ jade.js
@@ -1,25 +1,21 @@
 var jade  = require('jade')
 var fs    = require('fs')
 
-var jadeCompiled  = []
-var locals        = [
-  {
-    things: [
-      { a: 'blah', b: 'halb' }
-    , { a: 'clah', b: 'halc' }
-    , { a: 'clah', b: 'halc' }
-    , { a: 'blah', b: 'halb' }
-    , { a: 'clah', b: 'halc' }
-    , { a: 'clah', b: 'halc' }
-    , { a: 'clah', b: 'halc' }
-    , { a: 'blah', b: 'halb' }
-    , { a: 'blah', b: 'halb' }
-    , { a: 'clah', b: 'halc' }
-    , { a: 'blah', b: 'halb' }
-    ]
-  }
-, {}
+var jadeCompiled = []
+var things = [
+    { a: 'blah', b: 'halb' }
+  , { a: 'clah', b: 'halc' }
+  , { a: 'clah', b: 'halc' }
+  , { a: 'blah', b: 'halb' }
+  , { a: 'clah', b: 'halc' }
+  , { a: 'clah', b: 'halc' }
+  , { a: 'clah', b: 'halc' }
+  , { a: 'blah', b: 'halb' }
+  , { a: 'blah', b: 'halb' }
+  , { a: 'clah', b: 'halc' }
+  , { a: 'blah', b: 'halb' }
 ]
+var locals = [{things:things}, {things:things}, {things:things}, {things:things}, {}]
 
 var jadeOpts = {  }
 var jadeOpts = { compileDebug: false }
@@ -40,8 +36,8 @@ for (var i = 0; i < files.length; i ++) {
   var local = locals[i]
 
   var fn = jadeCompiled[i]
-  for (var j = 0; j < 1000000; j ++) fn(local)
+  for (var j = 0; j < 200000; j ++) fn(local)
   console.time(f)
-  for (var j = 0; j < 10000000; j ++) fn(local)
+  for (var j = 0; j < 2000000; j ++) fn(local)
   console.timeEnd(f)
 }
